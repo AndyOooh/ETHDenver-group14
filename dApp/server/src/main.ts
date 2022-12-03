aa5cbf9ddc409a8e776aa632bfb0155a8ee6fc2f
AndyOoh <andy.oeee@gmail.com>
2022-12-03 11:20:19 +0700

    wrapped swagger in if NODE_ENV = dev and moved @nestjs/config to dev

--- dApp/server/src/main.ts
+++ dApp/server/src/main.ts
@@ -7,16 +7,19 @@ async function bootstrap() {
   const {NODE_ENV, PORT, ORIGIN_DEV, ORIGIN_PROD} = process.env;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    // origin: NODE_ENV === 'production' ? ORIGIN_PROD : ORIGIN_DEV
+    origin: NODE_ENV === 'production' ? ORIGIN_PROD : ORIGIN_DEV
   });
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Tokenized Ballot API')
-    .setDescription('UI for interacting with NestJS API')
-    .setVersion('1.0')
-    .addTag('Group 14')
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+  if (NODE_ENV === 'development') {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('Tokenized Ballot API')
+      .setDescription('UI for interacting with NestJS API')
+      .setVersion('1.0')
+      .addTag('Group 14')
+      .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(PORT || 8080, () => {
     console.log(`Listening on Port ${PORT}`);
   });
