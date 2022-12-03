0bcf92d0e1efe73c3c9544e1a5aaa9e5d7852dec
AndyOoh <andy.oeee@gmail.com>
2022-12-03 11:43:03 +0700

    downgrade react types to 18.0.1

--- dApp/client/api/axios.config.ts
+++ dApp/client/api/axios.config.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASEURL =
   process.env.NODE_ENV === 'production'
-    ? 'https://encode-server.herokuapp.com'
+    ? 'https://encode-server.herokuapp.com/'
     : 'http://localhost:3500';
 
 export const api = axios.create({
