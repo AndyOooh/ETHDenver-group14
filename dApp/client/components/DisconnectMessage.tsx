e7ea92f7fbe92c660ed806389e9576bb3225d291
AndyOoh <andy.oeee@gmail.com>
2022-12-04 05:19:05 +0700

    type in DisconnectMessage

--- dApp/client/components/DisconnectMessage.tsx
+++ dApp/client/components/DisconnectMessage.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const DisconnectMessage: NextPage = () => {
   return (
     <div className="text-center">
-      <p className="text-xl">To use this app youb have to</p>
+      <p className="text-xl">To use this app you have to</p>
       <p className="text-3xl">
         connect your
         <span className="text-orange-400 "> metamask </span>
